ebac4705ab96c9838719dbadd210c07e424658fa
ManelCansecoPeris <106727838+ManelCansecoPeris@users.noreply.github.com>
2022-06-02 08:30:45 +0200

    Add files via upload

---
+++ cancons.js
@@ -0,0 +1,26 @@
+/* 
+- Les cançons han d'estar a la carpeta "audio"
+
+- Per introduir una cançó has de seguir aquest format
+
+    nom_del_arxiu.mp3 | Nom de la cançó \n\
+
+    * Recorda ficar | entre el nom de l'arxiu i el nom de la cançó
+    * Recorda ficar \n\ en acabar cada línia (excepte la última)
+
+*/
+const str_cansons = "Arxiu|Nom\n\
+0.mp3 | Mediterrània\n\
+1.mp3 | Plàstic Àiland\n\
+2.mp3 | Transmets Energia\n\
+3.mp3 | Això es pot salvar\n\
+4.mp3 | Si et quedes amb mi\n\
+5.mp3 | La Flama\n\
+6.mp3 | La manta al coll\n\
+7.mp3 | Les nits no moren mai\n\
+8.mp3 | Rotllo i canya\n\
+9.mp3 | Bon dia\n\
+10.mp3 | Caminem junts\n\
+11.mp3 | Invencibles \n\
+12.mp3 | Una lluna a l'aigua \n\
+13.mp3 | Himne del millor equip de món";
